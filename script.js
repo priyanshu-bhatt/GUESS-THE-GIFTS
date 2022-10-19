1bf7068819366c9d261923b41bd7b7e01aa69e11
priyanshu-bhatt <priyanshubhatt1892001@gmail.com>
2022-10-19 23:37:54 +0530

    Level1 completed

--- script.js
+++ script.js
@@ -17,21 +17,49 @@ const  gift1=Math.floor(Math.random() * 9)+1
         var loose_counter=0;
         var collection = document.getElementsByClassName("grid-item");
         ar = [1,2, 3,4,5,6,7,8,9];
-        
             function fun(){
+                //document.getElementById('lets-go').play();
+                
                 var input_value=parseInt(document.getElementById("inp1").value);
                 if((input_value==gift1 || input_value==gift2 || input_value==gift3) && ar.includes(input_value)){
+                    document.getElementById('lets-go').pause();
                     //console.log("gift!!")
                     //console.log(collection[input_value-1].innerHTML);
-                    
+                    //document.getElementById('lets-go').pause();
                     ar = ar.filter(item => !(item == collection[input_value-1].innerHTML));
                     console.log(ar)
-                    collection[input_value-1].innerHTML="Gift!!";
-                    collection[input_value-1].style.background="linear-gradient(to right, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #eeff41, #f9a825, #ff5722)";
+                    
+                    //collection[input_value-1].innerHTML="Gift!!";
+                    //collection[input_value-1].style.background="linear-gradient(to right, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #eeff41, #f9a825, #ff5722)";
+                    collection[input_value-1].style.background="none";
+                    collection[input_value-1].style.opacity=0;
+                    collection[input_value-1].innerHTML="R";
+                    document.getElementById('gift-found').play();
+                    setTimeout('document.getElementById("lets-go").play();', 1000);
+                    
+                    //document.getElementById('lets-go').pause();
+                    //document.getElementById('gift-found').play();
+                    //setTimeout('document.getElementById("lets-go").play();', 1000)
                      // [1, 2, 3]
                      win_counter++;
+
                      if(win_counter==3){
+                        //document.getElementById('lets-go').pause();
+                        //document.getElementById('gift-found').pause();
+                        //document.getElementById('redeem').play();
+                        collection[input_value-1].style.background="linear-gradient(to right, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #eeff41, #f9a825, #ff5722)";
+
+                        collection[input_value-1].style.opacity=1;
                         collection[input_value-1].innerHTML= "<a style='text-decoration:none;' href='#popup1'>Reedem Gift!!</a>";
+                        
+                        var lets=document.getElementById('lets-go')
+                       lets.volume=0;
+                       var found  =document.getElementById("gift-found");
+                       found.volume=0;
+                        //document.getElementById('gift-found').volume();
+                        document.getElementById('redeem').play();
+                        
+                        
                         document.getElementById("accuracy").innerHTML="<b>You took "+ (loose_counter+ win_counter) +"chances to Win</b>";
                         win_counter=0;
                     }
@@ -40,6 +68,7 @@ const  gift1=Math.floor(Math.random() * 9)+1
                     }
                    }
                    else{
+
                     loose_counter++;
                     console.log("it runs")
                     if(input_value>=10){
@@ -47,8 +76,12 @@ const  gift1=Math.floor(Math.random() * 9)+1
                         //window.location.reload();
                     }
                         else{
+                            
+                            
                     console.log("loose"+loose_counter)
+                    document.getElementById('loose').play();
                     alert("Duplicate Or Wrong Value given");
+                    
                     console.log("Duplicate Or Wrong Value given");
                           
                     }
@@ -59,12 +92,16 @@ const  gift1=Math.floor(Math.random() * 9)+1
         document.getElementById("glow").style.display = "none";
         document.getElementById("marq").style.display = "none";
         document.getElementById("main-button").style.display = "none";
+       
+        
         
         //document.getElementById("text").style.removeProperty('font-size');
         //document.getElementById("body").removeAttribute("class");
-        //document.getElementById("body").setAttribute("class", "bodycss"); 
-        
+        //document.getElementById("body").setAttribute("class", "bodycss");   
+    }
 
-}
+    
+    
+     
 
         
